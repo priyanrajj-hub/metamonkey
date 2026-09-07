85c2ff1291209d02cf76a50699b6ae038cd71347
Priyanraj <priyanrajj@gmail.com>
2026-09-07 13:04:43 +0530

    Fix URL encoding for Overpass API query to resolve 502 error

--- api/osm-lookup/index.js
+++ api/osm-lookup/index.js
@@ -11,7 +11,8 @@ module.exports = async function (req, res) {
         }
 
         const bbox = `${lat - 0.005},${lng - 0.005},${lat + 0.005},${lng + 0.005}`;
-        const overpassQuery = `https://overpass-api.de/api/interpreter?data=[out:json];(node(${bbox})["landuse"];way(${bbox})["landuse"];node(${bbox})["crop"];way(${bbox})["crop"];node(${bbox})["produce"];way(${bbox})["produce"];);out;`;
+        const queryData = `[out:json];(node(${bbox})["landuse"];way(${bbox})["landuse"];node(${bbox})["crop"];way(${bbox})["crop"];node(${bbox})["produce"];way(${bbox})["produce"];);out;`;
+        const overpassQuery = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(queryData)}`;
 
         const response = await fetch(overpassQuery, {
             headers: {
